f916c2dbdac18fcd9e991148f3ae370e7085b9f5
TaiND <taind@ai-t.vn>
2016-04-05 08:36:17 +0700

    Tìm hiểu follow xử lý dữ liệu của PHP Làm ví dụ về định nghĩa và xử lý dữ liệu với PHP Array: tạo một array chứa dữ liệu

---
+++ Project1/common/data.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: taind
+ * Date: 4/4/16
+ * Time: 16:21
+ */
---
+++ Project1/controllers/detail.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: taind
+ * Date: 4/4/16
+ * Time: 17:06
+ */
+class detail
+{
+
+}
